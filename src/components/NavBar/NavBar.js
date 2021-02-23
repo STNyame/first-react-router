566a453a870b08066a961c6ed3b8dc576b066798
STNyame <sentwalidors@hotmail.com>
2021-02-23 20:42:30 +0100

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import { Link, NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">- About this website -</NavLink>
+      <NavLink to="/discover">Discover Movies</NavLink>
+    </div>
+  );
+}
---
+++ src/pages/DiscoverMoviesPage.js
@@ -0,0 +1,3 @@
+export default function DiscoverMoviesPage() {
+  return <h1>Test</h1>;
+}
